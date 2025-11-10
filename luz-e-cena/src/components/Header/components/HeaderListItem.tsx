ed8a8cb5ed90562a0e74d65ca91164eb1c2f33c9
Victor <adsvictor@outlook.com>
2025-11-10 07:54:29 -0300

    ✨ feat: add Header component with styles and structure for header list and items

---
+++ luz-e-cena/src/components/Header/components/HeaderListItem.tsx
@@ -0,0 +1,7 @@
+import styles from "../Header.module.css";
+
+const HeaderListItem = ({ children }: React.HTMLAttributes<HTMLLIElement>) => {
+  return <li className={styles.menuitem}>{children}</li>;
+};
+
+export default HeaderListItem;
