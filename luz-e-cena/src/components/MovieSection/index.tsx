82885bcf3324e7887b13ddd930f603572f54895b
Victor <adsvictor@outlook.com>
2025-11-18 21:49:43 -0300

    ✨ feat: add useFilterMovies hook for movie search functionality in MovieSection

--- luz-e-cena/src/components/MovieSection/index.tsx
+++ luz-e-cena/src/components/MovieSection/index.tsx
@@ -1,5 +1,6 @@
 import { FaSearch } from "react-icons/fa";
-import useFetchMovies from "../../hooks/userFetchMovies";
+import useFetchMovies from "../../hooks/useFetchMovies";
+import useFilterMovies from "../../hooks/useFilterMovies";
 import Button from "../Button";
 import FieldSet from "../FieldSet";
 import InputText from "../InputText";
@@ -9,18 +10,33 @@ import styles from "./MovieSection.module.css";
 const MovieSection = () => {
   const { movies, isLoading, error } = useFetchMovies();
 
+  const { searchTerm, setSearchTerm, filteredMovies, handleSearch } =
+    useFilterMovies(movies);
+
   return (
     <main>
       <section className={styles.container}>
         <FieldSet variant="secondary">
-          <InputText placeholder="Buscar filmes..." />
-          <Button variant="icon">
+          <InputText
+            value={searchTerm}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setSearchTerm(event.target.value)
+            }
+            placeholder="Buscar filmes..."
+          />
+
+          <Button variant="icon" onClick={handleSearch}>
             <FaSearch />
           </Button>
         </FieldSet>
 
         <h1 className={styles.titulo}>Em cartaz</h1>
-        <MovieList movies={movies} />
+
+        {isLoading && <p>Carregando filmes...</p>}
+
+        {error && <p className={styles.error}>{error}</p>}
+
+        <MovieList movies={filteredMovies} />
       </section>
     </main>
   );
