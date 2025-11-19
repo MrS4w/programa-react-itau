da4230faff296a67db1347fde58150a2c205a40a
Victor <adsvictor@outlook.com>
2025-11-19 02:49:45 -0300

    ✨ feat: add Footer component with styling and subcomponents for improved layout

--- luz-e-cena/src/App.tsx
+++ luz-e-cena/src/App.tsx
@@ -1,4 +1,5 @@
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MovieSection from "./components/MovieSection";
 import Newsletter from "./components/Newsletter";
@@ -11,6 +12,7 @@ function App() {
       <MovieSection />
       <Banner src="./Imagens/Banner Combo Desktop.png" alt="Footer Banner" />
       <Newsletter />
+      <Footer />
     </>
   );
 }
