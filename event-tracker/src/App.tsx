924145e473369061c532643704cec8cbeec715a9
Victor <adsvictor@outlook.com>
2026-01-13 20:41:57 -0300

    ✨ feat: simplify event management by removing unused status alteration and event deletion functionalities

--- event-tracker/src/App.tsx
+++ event-tracker/src/App.tsx
@@ -8,51 +8,16 @@ import ListaDeEventos from "./components/ListaDeEventos";
 import { IEvento } from "./interfaces/IEvento";
 
 function App() {
-  // const [eventos, setEventos] = useState<IEvento[]>([
-  //   {
-  //     descricao: "Estudar React",
-  //     inicio: new Date("2022-01-15T09:00"),
-  //     fim: new Date("2022-01-15T13:00"),
-  //     completo: false,
-  //     id: 1642342747,
-  //   },
-  //   {
-  //     descricao: "Estudar Recoil",
-  //     inicio: new Date("2022-01-16T09:00"),
-  //     fim: new Date("2022-01-16T11:00"),
-  //     completo: false,
-  //     id: 1642342959,
-  //   },
-  // ]);
-
   const [filtro, setFiltro] = useState<Date | null>();
 
   const adicionarEvento = (evento: IEvento) => {
     evento.id = Math.round(new Date().getTime() / 1000);
-    // eventos.push(evento);
-    // console.log(eventos);
-
-    // setEventos([...eventos]);
   };
-  const alterarStatusEvento = (id: number) => {
-    // const evento = eventos.find((evento) => evento.id === id);
-    // if (evento) {
-    //   evento.completo = !evento.completo;
-    // }
-    // setEventos([...eventos]);
-  };
+  const alterarStatusEvento = (id: number) => {};
 
   const aplicarFiltro = (data: Date | null) => {
     setFiltro(data);
   };
-
-  // const filtrados = !filtro
-  //   ? eventos
-  //   : eventos.filter(
-  //       (evento) =>
-  //         filtro!.toISOString().slice(0, 10) ===
-  //         evento.inicio.toISOString().slice(0, 10)
-  //     );
 
   return (
     <RecoilRoot>
@@ -63,10 +28,7 @@ function App() {
           </Card>
           <hr />
           <Card>
-            <ListaDeEventos
-              aoFiltroAplicado={aplicarFiltro}
-              aoAlterarStatus={alterarStatusEvento}
-            />
+            <ListaDeEventos aoFiltroAplicado={aplicarFiltro} />
           </Card>
         </div>
         <div className={style.Coluna}>
